99ffd487e868ba467967318547efa252e8c717f2
marcelo <mrcel83@gmail.com>
2023-02-15 10:58:41 +0000

    converting to TypeScript

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,10 +6,13 @@ require("solidity-coverage")
 require("prettier-plugin-solidity")
 require("@nomiclabs/hardhat-etherscan")
 require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL 
-const PRIVATE_KEY = process.env.PRIVATE_KEY 
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "http://eth-goerli"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 
 module.exports = {
   defaultNetwork: "hardhat", 
@@ -20,9 +23,20 @@ module.exports = {
       accounts: [PRIVATE_KEY],
       chainId: 5,
     },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
   },
   solidity: "0.8.8",
   etherscan: {
     apiKey: ETHERSCAN_API_KEY
   },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  }
 };
